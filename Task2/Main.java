4ccac17d102fee94072047aff9dc382d5258e016
DariaYakkola <124380312+DariaYakkola@users.noreply.github.com>
2023-08-08 16:19:36 +0300

    Update Main.java

--- Task2/Main.java
+++ Task2/Main.java
@@ -2,7 +2,7 @@ package Homework.Task2;
 
 public class Main {
     public static void main(String[] args) {
-        int d = 8;
+        int d = 0;
         if (d == 0) {
             System.out.println("На 0 делить нельзя! ");
         } else {
